1a36e48f38842d7a786d89fd7e5d00c108c140a2
Tainara Novaes <tainarasn.sba@gmail.com>
2024-01-03 16:09:40 -0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,6 @@ import react from "@vitejs/plugin-react-swc"
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    build: {
-        chunkSizeWarningLimit: 1000, // Ajuste o limite para um valor adequado
-    },
+
     base: "/portfolio",
 })
